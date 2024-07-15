98650b1d97532a098ed7b52319562cb09bcf2319
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2024-07-15 16:59:08 +0000

    fixed bug in eas build

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,22 @@
-// // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-config.transformer.babelTransformerPath = require.resolve(
-  "react-native-svg-transformer"
-);
-config.resolver.sourceExts.push("cjs");
-config.resolver.sourceExts.push("svg");
-// config.resolver.assetExts.push("svg");
-config.transformer.minifierConfig = {
-  compress: {
-    // The option below removes all console logs statements in production.
-    drop_console: true,
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...sourceExts, "svg"],
   },
 };
-module.exports = config;
+
+module.exports = mergeConfig(defaultConfig, config);
